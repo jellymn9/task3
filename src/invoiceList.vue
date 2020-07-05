89dab81b3ac6fa93367e8d39e893ad72c4278715
Jelena Mandzic <jelena.mandzic@gmail.com>
2020-07-05 21:59:36 +0200

    findMatch function finished

--- src/invoiceList.vue
+++ src/invoiceList.vue
@@ -5,11 +5,13 @@
       <label for="idSearch">Search:</label>
       <input type="number" id="idSearch"  v-on:input="findMatch" ref="searchInput">
       <ul id="listId">
-        
-          <li v-show="pshow" v-for="invoice in this.$store.state.invoiceList"  > 
-            {{ invoice.getNumber}}. -description: {{ invoice.getDescription}} 
-          </li>
-        
+          <div >
+            
+            <li v-show="pshow" v-for="invoice in this.$store.state.invoiceList"> 
+              {{ invoice.getNumber}}. -description: {{ invoice.getDescription}} 
+            </li>
+          </div>
+          <div v-show="!pshow" ref="matchElement">{{val}}</div>
       </ul>
   </div>
   
@@ -20,7 +22,9 @@ export default {
   data() {
    return{
       // tables:['jjj','vvvv','kkkk']
-      pshow: true
+      pshow: true,
+      val:'',
+      //timeOutID: 0
    }
   },
   methods:{
@@ -31,19 +35,30 @@ export default {
       //console.log(this.pshow);
     },
     findMatch(){
-      //pshow = false;
+      this.pshow = true;
       this.$store.state.invoiceList.filter((invoice)=>{ 
-        this.pshow = false;
+        //this.pshow = false;
         console.log(this);
         if(invoice.getNumber == this.$refs.searchInput.value){
-          this.pshow = true;
+          this.val = ` ${invoice.getNumber} . -description: ${invoice.getDescription}`;
+          this.$refs.matchElement.value = this.val;
+          this.pshow = false;
           console.log("match is theree");
           console.log(this);
-          console.log(this.pshow);
+          console.log(this.$refs.matchElement.value);
 
         }
       })
-
+    },
+    debounce(f){
+        let timeOutID;
+        if(timeOutID){
+          clearTimeout(timeOutID);
+        }
+        timeOutID = setTimeout(f, 3000);
+        return function(){
+          
+        }
     }
   },
   computed:{
